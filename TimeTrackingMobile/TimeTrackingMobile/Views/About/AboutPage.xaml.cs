78bf1e3dbb95403d8e06bd753a84a3c13a23bb68
Michal <m.p.garnuch@gmail.com>
2025-04-26 22:34:08 +0200

    kreatywne rozwiązanie warto zapisać

--- TimeTrackingMobile/TimeTrackingMobile/Views/About/AboutPage.xaml.cs
+++ TimeTrackingMobile/TimeTrackingMobile/Views/About/AboutPage.xaml.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace TimeTrackingMobile.Views
 {
     public partial class AboutPage : ContentPage
     {
+        public IList<string> Screenshots { get; }
+
         public AboutPage()
         {
             InitializeComponent();
+
+            // Nazwy plików obrazów w Resources/Images
+            Screenshots = new List<string>
+            {
+                "screen1.png",
+                "screen2.png",
+                "screen3.png"
+            };
+            BindingContext = this;
         }
     }
 }
--- TimeTrackingMobile/TimeTrackingMobile/Views/Departments/DepartmentsPage.xaml.cs
+++ TimeTrackingMobile/TimeTrackingMobile/Views/Departments/DepartmentsPage.xaml.cs
@@ -1,103 +1,44 @@
 ﻿using Xamarin.Forms;
 using TimeTrackingMobile.Models;
 using TimeTrackingMobile.Services;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
-using System;
 
 namespace TimeTrackingMobile.Views
 {
     public partial class DepartmentsPage : ContentPage
     {
         private readonly DepartmentService _deptService;
-        private List<DepartmentModel> _departments;
+
+        // ✚ zastępujemy "new()" explicite: 
+        public ObservableCollection<DepartmentModel> Departments { get; }
+            = new ObservableCollection<DepartmentModel>();
+
+        public Command LoadDepartmentsCommand { get; }
 
         public DepartmentsPage()
         {
             InitializeComponent();
             _deptService = new DepartmentService();
+
+            LoadDepartmentsCommand = new Command(async () => await ExecuteLoadDepartments());
+
+            BindingContext = this;
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            await LoadDepartments();
+            if (Departments.Count == 0)
+                await ExecuteLoadDepartments();
         }
 
-        private async void LoadDepartmentsClicked(object sender, EventArgs e)
+        private async Task ExecuteLoadDepartments()
         {
-            await LoadDepartments();
-        }
-
-        private async Task LoadDepartments()
-        {
-            try
-            {
-                _departments = await _deptService.GetAllDepartments();
-                DepartmentsList.ItemsSource = _departments;
-                HeaderLabel.Text = $"Działy ({_departments.Count})";
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Błąd", ex.Message, "OK");
-            }
-        }
-
-        private async void AddDepartmentClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync(nameof(DepartmentAddPage));
-        }
-
-        private async void DepartmentsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem is DepartmentModel dept)
-            {
-                var action = await DisplayActionSheet(
-                    $"Wybrano: {dept.DepartmentName}",
-                    "Cancel",
-                    null,
-                    "Pokaż pracowników",
-                    "Edytuj dział",
-                    "Usuń dział");
-
-                if (action == "Pokaż pracowników")
-                {
-                    await Shell.Current.GoToAsync($"{nameof(EmployeesPage)}?departmentId={dept.DepartmentID}");
-                }
-                else if (action == "Edytuj dział")
-                {
-                    await Shell.Current.GoToAsync($"{nameof(DepartmentEditPage)}?departmentId={dept.DepartmentID}");
-                }
-                else if (action == "Usuń dział")
-                {
-                    bool confirmed = await DisplayAlert("Potwierdź", $"Usunąć '{dept.DepartmentName}'?", "Tak", "Nie");
-                    if (confirmed)
-                    {
-                        bool deleted = await _deptService.DeleteDepartment(dept.DepartmentID);
-                        if (deleted)
-                        {
-                            await DisplayAlert("OK", "Usunięto dział", "OK");
-                            await LoadDepartments();
-                        }
-                        else
-                        {
-                            await DisplayAlert("Błąd", "Nie udało się usunąć", "OK");
-                        }
-                    }
-                }
-
-                DepartmentsList.SelectedItem = null;
-            }
-        }
-
-        private async void OnSyncClicked(object sender, EventArgs e)
-        {
-            await LoadDepartments();
-        }
-
-        private async void OnLogoutClicked(object sender, EventArgs e)
-        {
-            await Shell.Current.GoToAsync("//LoginPage");
+            Departments.Clear();
+            var list = await _deptService.GetAllDepartments();
+            foreach (var d in list)
+                Departments.Add(d);
         }
     }
 }
